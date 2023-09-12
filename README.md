1f30bac2089110a68fb01e2707f1e552d1f108f3
Sebastian Fischer <sebf.fischer@gmail.com>
2023-09-12 16:30:07 +0200

    create benchmarking suite

--- README.md
+++ README.md
@@ -6,14 +6,18 @@ The package structure is as follows:
 * `./experiments` contains the scripts used to run the experiments
 
 
+## Benchmarking Suite
+
+The benchmarking suite is available on OpenML as a collection with [ID 396](https://www.openml.org/s/396).
+
 ## Reproducing the Experiments
 
 
-  
+
 
 1. Install the `renv` package and restore the computational environment using `renv::restore()`.
 1. Adjust the configuration: To be able to run the experiments on a different cluster, the following options have to be adjusted:
-   * 
+   *
 
    * Adjust the batchtools configuration in `batchtools.conf.R`, to your cluster.
      This includes changing the template `slurm_wyoming.tmpl`.
---
+++ misc/upload_collection.R
@@ -0,0 +1,41 @@
+devtools::load_all("~/mlr/mlr3oml")
+
+dt = read.csv("~/gh/paper_2023_ci_for_ge/data_ids.csv")
+data_ids = dt$data_id
+task_types =  dt$task_type
+
+task_ids = list()
+
+for (i in seq_along(data_ids)) {
+
+  type = task_types[i]
+  id = data_ids[i]
+  odata = odt(id)
+
+  if (type == "regr") {
+    estimation_procedure = 7
+  } else {
+    estimation_procedure = 1
+  }
+
+  task_id = publish_task(
+    id = data_ids[i],
+    type = task_types[i],
+    estimation_procedure = estimation_procedure,
+    target = odata$target_names
+    
+  )
+
+  task_ids = append(task_ids, task_id)
+}
+
+publish_collection(
+  unlist(task_ids),
+  name = "CI for GE benchmark",
+  desc = paste0(
+    "This is a collection of datasets that can be used to evaluate Confidence Interval methods for the Generalization Error.",
+    "The task splits can be ignored.",
+    "For more information, see [https://github.com/slds-lmu/paper_2023_ci_for_ge](https://github.com/slds-lmu/paper_2023_ci_for_ge)."
+  ),
+  test_server = FALSE
+)
