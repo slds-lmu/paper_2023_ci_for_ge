c3345bf76d82eb186995853fb4027c7f1c3aa1b7
Sebastian Fischer <sebf.fischer@gmail.com>
2024-05-02 17:22:22 +0200

    austern zhou

---
+++ analysis/az.R
@@ -0,0 +1,52 @@
+library(mlr3)
+library(mlr3misc)
+library(mlr3learners)
+library(data.table)
+library(inferGE)
+library(here)
+
+future::plan("multicore")
+
+lgr::get_logger("mlr3")$set_threshold("warn")
+
+SIZES = c(500L, 1000L, 5000L)
+REPS_AZ = 10L
+REPS_TRUTH = 100L
+
+set.seed(314L)
+
+simulate = function(n) {
+  x = rnorm(n)
+  y = x + rnorm(n)
+  dat = data.table(x = x, y = y)
+  as_task_regr(dat, target = "y")
+}
+
+var_ests = map(SIZES, function(n) {
+  var_ests = map_dbl(1:REPS_AZ, function(rep) {
+    task = simulate(n)
+    res_az = rsmp("austern_zhou", folds = 5L)
+    res_cv = rsmp("cv", folds = 5L)
+    rr_az_lm = resample(task, lrn("regr.lm"), res_az)
+    rr_cv_lm = resample(task, lrn("regr.lm"), res_cv)
+    ci_az_lm = infer_austern_zhou(rr_az_lm, rr_cv_lm)
+    sqrt(ci_az_lm$info[[1L]]$s2_cv / n)
+  })
+
+  data.table(n = n, est = mean(var_ests), se_est = sd(var_ests) / sqrt(REPS_AZ))
+}) |> rbindlist()
+
+
+truths = map(SIZES, function(n) {
+  truth_ests = map_dbl(1:REPS_TRUTH, function(i) {
+    task = simulate(n)
+    resampling = rsmp("cv", folds = 5)$instantiate(task)
+    unname(resample(task, lrn("regr.lm"), resampling)$aggregate())
+  })
+
+  data.table(n = n, truth = mean(truth_ests), se_truth = sd(truth_ests) / sqrt(REPS_TRUTH))
+}) |> rbindlist()
+
+dt = merge(var_ests, truths, by = "n")
+
+saveRDS(dt, here("results", "az", "ratio.rds"))
