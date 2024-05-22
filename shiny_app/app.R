9e0df76871d10a31b1a3fe19708a84db48895d13
Sebastian Fischer <sebf.fischer@gmail.com>
2024-05-22 14:23:36 +0200

    add CIs for coverage of CIs

--- shiny_app/app.R
+++ shiny_app/app.R
@@ -14,6 +14,7 @@ source("plot_specifications.R")
 source("Size_x_Coverage.R")
 source("target_comparison.R")
 source("width_vs_coverage.R")
+source("cis_for_cis.R")
 
 ui = fluidPage(
   # Add a custom CSS for the banner
@@ -151,7 +152,9 @@ plotPage = fluidPage(
     tabPanel("Width vs. Coverage", fluidPage(
       specification_width_vs_coverage("width_vs_coverage")
     )),
-    tabPanel("CIs for coverage", fluidPage()),
+    tabPanel("CIs for CI Coverage", fluidPage(
+      specification_cis_for_cis("cis_for_cis")
+    )),
     tabPanel("Austern & Zhou", fluidPage()),
     specifications_download("downloadNS"))
   )
@@ -228,6 +231,10 @@ server = function(input, output, session) {
 
   observeEvent(input$Vwidth_vs_coverage, {
     button_clicked("VIEW_width_vs_coverage")
+  })
+
+  observeEvent(input$Vcis_for_cis, {
+    button_clicked("VIEW_cis_for_cis")
   })
   
   observeEvent(input$Vtask, {
@@ -370,6 +377,30 @@ server = function(input, output, session) {
     )
     
   }, "width_vs_coverage")
+
+  callModule(function(input, output, session) {
+    output$Pcis_for_cis = renderPlotly({
+      clicker = button_clicked()
+      g = make_cis_for_cis_plot(ci_aggr, input)
+      makeplot(clicker, "VIEW_cis_for_cis", g)
+    })
+    
+    output$Dcis_for_cis = downloadHandler(
+      filename = function() {
+        "plot.png"
+      },
+      content = function(file) {
+        g = make_cis_for_cis_plot(ci_aggr, input)
+        print(addon_applied)
+        print(global_units)
+        if (!is.null(addon_applied)) {
+          g = g + eval(parse(text = global_code))
+        }
+        ggsave(file, plot = g, width = as.numeric(global_width), height = as.numeric( global_height), device = "png", units = global_units)
+      }
+    )
+    
+  }, "cis_for_cis")
   
   
   callModule(function(input, output, session) {
