b6cc7cf4c9129fa4cc18904104d5befe1c46412d
Sebastian Fischer <sebf.fischer@gmail.com>
2024-05-22 15:22:09 +0200

    ...

--- shiny_app/cis_for_cis.R
+++ shiny_app/cis_for_cis.R
@@ -4,38 +4,41 @@ specification_cis_for_cis <- function(id) {
     "CIs for CIs",
     div(
       class = "content",
-    sidebarLayout(
-      sidebarPanel(
-        fluidRow(
-          column(6,
-            selectInput(ns("size"), "Size:", choices = as.character(c(100L, 500L, 1000L, 5000L, 10000L)), "100"),
-            selectInput(ns("learner"), "Learner:", choices = LEARNERS),
-            selectInput(ns("task"), "Task:", choices = TASKS),
-            selectInput(ns("target"), "Target:", choices = c("Risk", "Expected Risk"), "Risk"),
-            pickerInput(ns("methods"), "Methods:",
-                        choices = METHODS,
-                        multiple = TRUE,
-                        options = list(`actions-box` = TRUE),
-                        selected = METHODS),
-            br(),
-            actionButton("Vcis_for_cis", "View Plot"),
-            downloadButton(ns("Dcis_cor_cs"), "Download Plot")
+      sidebarLayout(
+        sidebarPanel(
+          fluidRow(
+            column(
+              6,
+              selectInput(ns("size"), "Size:", choices = as.character(c(100L, 500L, 1000L, 5000L, 10000L)), "100"),
+              selectInput(ns("learner"), "Learner:", choices = LEARNERS),
+              selectInput(ns("task"), "Task:", choices = TASKS),
+              selectInput(ns("target"), "Target:", choices = c("Risk", "Expected Risk"), "Risk"),
+              pickerInput(ns("methods"), "Methods:",
+                choices = METHODS,
+                multiple = TRUE,
+                options = list(`actions-box` = TRUE),
+                selected = METHODS
+              ),
+              br(),
+              actionButton("Vcis_for_cis", "View Plot"),
+              downloadButton(ns("Dcis_cor_cs"), "Download Plot")
+            )
+          )
+        ),
+        mainPanel(
+          div(
+            class = "plot-container",
+            plotlyOutput(ns("Pcis_for_cis"))
           )
         )
-      ),
-      mainPanel(
-        div(
-          class = "plot-container",
-          plotlyOutput(ns("Pcis_for_cis")) 
-        )
       )
-    ))
+    )
   )
 }
 
 
-make_cis_for_cis_plot = function(data, input) {
-  data = data[
+make_cis_for_cis_plot <- function(data, input) {
+  data <- data[
     learner == input$learner &
       task == input$task &
       size == input$size &
@@ -43,7 +46,7 @@ make_cis_for_cis_plot = function(data, input) {
   ]
   saveRDS(data, "~/scratch/dat.rds")
   data[, let(method = droplevels(method))]
-  cov_var = if (identical(input$target, "Risk")) "cov_R" else "cov_ER" 
+  cov_var <- if (identical(input$target, "Risk")) "cov_R" else "cov_ER"
   data[, let(
     cov = get(cov_var),
     upper = get(cov_var) + 1.96 * get(paste0(cov_var, "_se")),
@@ -52,8 +55,6 @@ make_cis_for_cis_plot = function(data, input) {
   head(data)
 
   ggplot(data) +
-    geom_point(aes(y = method, x = cov, color = method)) + 
+    geom_point(aes(y = method, x = cov, color = method)) +
     geom_errorbar(aes(y = method, xmin = lower, xmax = upper, color = method))
 }
-
-
