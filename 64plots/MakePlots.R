2f06d3368c1eed07ac85569060ddcee65b7c6a65
Hannah Schulz-Kümpel <80972004+11annah@users.noreply.github.com>
2024-06-11 09:22:41 +0200

    meta statistics

--- 64plots/MakePlots.R
+++ 64plots/MakePlots.R
@@ -7,132 +7,75 @@ library(viridis)
 
 source("shiny_app/setup.R")
 source("64plots/helpers.R")
-
-## Edited fallback_plot
-
-make_64plots = function(data,
-  input_y, input_range, input_size,
-  input_evaluation,
-  input_loss_regr, input_loss_classif,
-  input_free_scales = "none",
-  methods, dgps, inducers,
-  sep_reg_class = FALSE) {
-  data = data[as.character(method) %in% methods & as.character(dgp) %in% dgps & as.character(inducer) %in% inducers]
-  y = switch(input_y,
-    "Risk" = "y_R", "Expected Risk" = "y_ER",
-    "Proxy Quantity" = "y_PQ"
-  )
-
-  scales = translate_scales(input_free_scales)
-
-  vec = c("inducer", "dgp", "method", "size")
-
-  newdat = if (input_evaluation == "Coverage Error") {
-    data[size == input_size & measure %in% translate_losses(input_loss_regr, input_loss_classif),
-      list(
-        y_R = sqrt(mean((cov_R - 0.95)^2)),
-        y_ER = sqrt(mean((cov_ER - 0.95)^2)),
-        y_PQ = sqrt(mean((cov_PQ - 0.95)^2)),
-        width = width,
-        iters = iters
-      ), by = vec]
-  } else {
-    data[size == input_size & measure %in% translate_losses(input_loss_regr, input_loss_classif),
-      list(
-        y_R = mean(cov_R),
-        y_ER = mean(cov_ER),
-        y_PQ = mean(cov_PQ),
-        width = width,
-        iters = iters
-      ), by = vec]
-  }
-
-  plotdata = newdat[DATA_OVERVIEW, on = c(dgp = "name_short"), nomatch = 0]
-  plotdata[, let(
-    stand_width = round(width / .SD[method == "bayle_10_all_pairs", "width"][[1L]],2)
-  ), by = "dgp"]
-  plotdata$numwidth = as.character(plotdata$stand_width)
-
-  breaks = c(0, 0.5, 1, 1.5, max(plotdata$stand_width))
-  colors = c("darkgreen", "darkblue", "purple", "red")
-  ####
-
-
-  breaks = c(0, 0.5, 1, 2, 3, max(plotdata$stand_width))
-  colors = c("darkgreen", "darkblue", "orange", "purple", "red")
-  ####
-
-  p = make_baseplot(plotdata, y, input_evaluation, input_range, inducers, scales, colors, breaks)
-
-
-  if (length(inducers) == 1) {
-    if (!sep_reg_class) {
-      output = p + facet_wrap(vars(method))
-    } else {
-      output = p + facet_grid(task_type ~ method)
-    }
-  } else {
-    output = p + facet_wrap(vars(method, inducer))
-  }
-
-  output = output +
-    labs(
-      x = input_evaluation,
-      y = "DGP",
-      shape = "DGP type"
-    ) +
-    theme_bw() +
-    theme(legend.position = "bottom", legend.box = "vertical") +
-    guides(color = guide_colorbar(title = "CI width rel. to Bayle (10)",
-      barwidth = 30,
-      barheight = 0.5))
-
-  return(output)
-}
-
-make_64plots(data = ci_aggr,
-  input_y = "Expected Risk", input_range = c(0, 1), input_size = 500,
-  input_evaluation = "Coverage Frequency",
-  input_loss_regr = "Squared", input_loss_classif = "Zero-One",
-  methods = setdiff(DEFAULT_METHODS, c("ls_bootstrap_100", "ts_bootstrap")), dgps = DGPS, inducers = "lm_or_logreg") + ggtitle("Plot for expected risk, classic loss and lm/logistic regression")
-ggsave("64plots/PNGs/ER_lmlog.png", width = 9, height = 14)
-
-make_64plots(data = ci_aggr,
-  input_y = "Expected Risk", input_range = c(0, 1), input_size = 500,
-  input_evaluation = "Coverage Frequency",
-  input_loss_regr = "Squared", input_loss_classif = "Zero-One",
-  methods = setdiff(DEFAULT_METHODS, c("ls_bootstrap_100", "ts_bootstrap")), dgps = DGPS, inducers = "decision_tree") + ggtitle("Plot for expected risk, classic loss and decision tree")
-ggsave("64plots/PNGs/ER_decisiontree.png", width = 9, height = 14)
-
-make_64plots(data = ci_aggr,
-  input_y = "Risk", input_range = c(0, 1), input_size = 500,
-  input_evaluation = "Coverage Frequency",
-  input_loss_regr = "Squared", input_loss_classif = "Zero-One",
-  methods = DEFAULT_METHODS, dgps = DGPS, inducers = INDUCERS,
-  sep_reg_class = FALSE)
-make_64plots(data = ci_aggr,
-  input_y = "Risk", input_range = c(0, 1), input_size = 100,
-  input_evaluation = "Coverage Frequency",
-  input_loss_regr = "Squared", input_loss_classif = "Zero-One",
-  methods = DEFAULT_METHODS, dgps = DGPS, inducers = "random_forest",
-  sep_reg_class = FALSE) + ggtitle(
-  paste("inducer: ", "random_forest", ", size: ", 100, ", target: ", "Risk", ", loss: L2/0-1")
-)
-
-ggsave(here("64plots", "PNGs", "test.png"), width = 12, height = 14)
+source("64plots/Plotfuns.R")
 
 inducers = unique(ci_aggr$inducer)
 sizes = unique(ci_aggr$size)
 
+stand = "mean" #or "Bayle"
+
+### getting info
+stand_loss_list_DGPs <- list()
+stand_loss_list_methods <- list()
+
+for (size in sizes) {
 for (inducer in inducers) {
-  for (size in sizes) {
     for (input_y in c("Risk", "Expected Risk")) {
-      make_64plots(data = ci_aggr,
+      output <- make_64plots(data = ci_aggr,
+                             input_y = input_y, input_range = c(0, 1), input_size = size,
+                             input_evaluation = "Coverage Frequency",
+                             input_loss_regr = "Squared", input_loss_classif = "Zero-One",
+                             methods = DEFAULT_METHODS, dgps = DGPS, inducers = inducer,
+                             sep_reg_class = FALSE, stand=stand) 
+      initialize_nested_list(stand_loss_list_DGPs, c(as.character(size), inducer))
+      initialize_nested_list(stand_loss_list_methods, c(as.character(size), inducer))
+      
+      stand_loss_list_DGPs[[as.character(size)]][[inducer]][[input_y]] <- output[["suspicious_DGPs"]]
+      stand_loss_list_methods[[as.character(size)]][[inducer]][[input_y]] <- as.data.frame(output[["info"]])
+    
+      }
+  }
+}
+
+other_loss_list_DGPs <- list()
+other_loss_list_methods <- list()
+
+
+for (size in sizes) {
+for (inducer in inducers) {
+    for (input_y in c("Risk", "Expected Risk")) {
+      for (loss in unlist(LOSSES)) {
+        loss_t = translate_losses(loss)
+        output <- make_64plots(data = ci_aggr,
+                               input_y = "Risk", input_range = c(0, 1), input_size = size,
+                               input_evaluation = "Coverage Frequency",
+                               input_loss_regr = loss, input_loss_classif = loss,
+                               methods = DEFAULT_METHODS, dgps = DGPS, inducers = inducer,
+                               sep_reg_class = FALSE, stand=stand)  
+        initialize_nested_list(stand_loss_list_DGPs, c(as.character(size), inducer,input_y))
+        initialize_nested_list(stand_loss_list_methods, c(as.character(size), inducer,input_y))
+        
+        
+        other_loss_list_DGPs[[as.character(size)]][[inducer]][[input_y]][[loss]] <- output[["suspicious_DGPs"]]
+        other_loss_list_methods[[as.character(size)]][[inducer]][[input_y]][[loss]] <- as.data.frame(output[["info"]])
+      }
+    }
+  }
+}
+
+
+        
+#### Making Plots
+for (size in sizes) {
+for (inducer in inducers) {
+    for (input_y in c("Risk", "Expected Risk")) {
+      output <- make_64plots(data = ci_aggr,
         input_y = input_y, input_range = c(0, 1), input_size = size,
         input_evaluation = "Coverage Frequency",
         input_loss_regr = "Squared", input_loss_classif = "Zero-One",
         methods = DEFAULT_METHODS, dgps = DGPS, inducers = inducer,
-        sep_reg_class = FALSE) + ggtitle(
+        sep_reg_class = FALSE, stand=stand) 
+      output[["plot"]] + + ggtitle(
         paste("inducer: ", inducer, ", size: ", size, ", target: ", input_y, ", loss: L2/0-1")
       )
       ggsave(here("64plots", "PNGs", "classic_losses", paste0(inducer, "_", size, "_", gsub(" ", "", input_y), ".png")), width = 12, height = 14)
@@ -142,18 +85,18 @@ for (inducer in inducers) {
 
 # not also for the different loss functions. Here we need to split by task type, otherwise we have 3 * 4 = 12 combinations
 
-
+for (size in sizes) {
 for (inducer in inducers) {
-  for (size in sizes) {
     for (input_y in c("Risk", "Expected Risk")) {
       for (loss in unlist(LOSSES)) {
         loss_t = translate_losses(loss)
-        make_64plots(data = ci_aggr,
+        output <- make_64plots(data = ci_aggr,
           input_y = "Risk", input_range = c(0, 1), input_size = size,
           input_evaluation = "Coverage Frequency",
           input_loss_regr = loss, input_loss_classif = loss,
           methods = DEFAULT_METHODS, dgps = DGPS, inducers = inducer,
-          sep_reg_class = FALSE) + ggtitle(
+          sep_reg_class = FALSE, stand=stand)
+        output[["plot"]] + ggtitle(
           paste("inducer: ", inducer, ", size: ", size, ", target: ", input_y, ", loss: ", loss)
         )
         ggsave(here("64plots", "PNGs", "other_losses", paste0(inducer, "_", size, "_", gsub(" ", "", input_y), "_", loss_t, ".png")), width = 12, height = 14)
--- 64plots/helpers.R
+++ 64plots/helpers.R
@@ -29,3 +29,87 @@ make_baseplot = function(plotdata, y, input_evaluation, input_range, inducers, s
   }
   return(output)
 }
+
+
+over_under <- function(x,coverage = c("over", "under"),alpha) {
+  # Ensure 'choice' is one of the allowed values
+  coverage <- match.arg(coverage)
+  
+  # Your function logic here
+  if (coverage == "under") {
+    x_under <- x[which(x<(1-alpha))]
+    if(length(x_under)==0){return(0)
+    }else{
+    return(sum((1-alpha)-x_under)/((1-alpha)*length(x_under)))
+    }
+  } else {
+    x_over <- x[which(x>(1-alpha))]
+    if(length(x_over)==0){return(0)
+    }else{
+    return(sum(x_over-(1-alpha))/(alpha*length(x_over)))
+    }
+  }
+}
+
+over_under_cutoff <- function(x,coverage = c("over", "under"),alpha,cutoff){
+  coverage <- match.arg(coverage)
+  x_new <- x[which(x>cutoff)]
+  n <- length(x_new)
+  value <- ifelse(n>0,over_under(x_new,coverage,alpha),NA)
+  return(list(value=value,n=n))
+}
+
+
+
+
+find_suspicious_dgps <- function(plotdata,y,cutoff,cutoff_method_check,CI_width){
+
+  lower_than_cutoff <- unique(plotdata[plotdata[[y]]<cutoff & method %in%cutoff_method_check,]$dgp)
+  if(length(lower_than_cutoff)>0){
+    D1 <- data.frame(name=lower_than_cutoff,
+                     reason=paste0("y<",cutoff," for solid methods"))
+  }else{D1 <- data.frame(name="none",reason="none")}
+  
+  larger_than_CI_width <- plotdata[,list(mean_width=mean(width)),by="dgp"]
+  larger_than_CI_width <- larger_than_CI_width[which(plotdata[,list(mean_width=mean(width)),by="dgp"]$mean_width>CI_width),]
+  if(nrow(larger_than_CI_width)>0){
+    D2 <- data.frame(name=unique(larger_than_CI_width$dgp),
+                     reason=paste0("average width of",unique(larger_than_CI_width$mean_width)," (>",CI_width,")"))
+  }else{D2 <- data.frame(name="none",reason="none")}
+  return(rbind(D1,D2))
+}
+
+
+initialize_nested_list <- function(lst, keys) {
+  for (key in keys) {
+    if (is.null(lst[[key]])) {
+      lst[[key]] <- list()
+    }
+    lst <- lst[[key]]
+  }
+}
+
+flatten_nested_list <- function(lst, parent_names = list()) {
+  do.call(rbind, lapply(names(lst), function(name) {
+    current_data <- lst[[name]]
+    current_parent_names <- c(parent_names, name)
+    
+    if (is.data.frame(current_data)) {
+      # Create a data frame from parent names
+      parent_df <- as.data.frame(t(current_parent_names), stringsAsFactors = FALSE)
+      colnames(parent_df) <- paste0("Level", seq_along(current_parent_names))
+      
+      # Combine with the current data frame
+      combined_df <- cbind(parent_df, current_data)
+      return(combined_df)
+    } else {
+      # Recursively process nested lists
+      return(flatten_nested_list(current_data, current_parent_names))
+    }
+  }))
+}
+
+
+
+
+
