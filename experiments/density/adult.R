cb3edb7cba565ab8dc822749c8364976de91f154
Sebastian Fischer <sebf.fischer@gmail.com>
2024-06-21 12:11:14 +0200

    ...

--- experiments/density/adult.R
+++ experiments/density/adult.R
@@ -9,10 +9,33 @@ library(mlr3pipelines)
 
 odata = odt(1590)
 
-task = as_task(odata)
-chunks = data.table(job.id = ids, chunk = batchtools::chunk(ids, chunk.size = 100))
+data = odata$data[complete.cases(odata$data)]
+
+task = as_task_classif(data, id = "adult", target = "class")
+
+
 learner = lrn("classif.log_reg")
 
-graph = ppl("robustify", learner = learner, task = task)
+learner = as_learner(
+  ppl("robustify", learner = learner, task = task) %>>%
+    learner
+)
 
-taskout = graph$train(task)[[1L]]
+logs = map(1:10, function(i) {
+  task = task$clone(deep = TRUE)
+  task$filter(sample(task$row_ids, 1000L))
+  learner$train(task)
+  learner$predict(task)
+})
+
+# learner$train(task)
+
+
+# task = as_task(odata)
+# data = 
+# chunks = data.table(job.id = ids, chunk = batchtools::chunk(ids, chunk.size = 100))
+# learner = lrn("classif.log_reg")
+
+# graph = ppl("robustify", learner = learner, task = task)
+
+# taskout = graph$train(task)[[1L]]
--- experiments/density/run.R
+++ experiments/density/run.R
@@ -8,88 +8,79 @@ library(data.table)
 library(mlr3pipelines)
 library(batchtools)
 
-set.seed(1)
-
-size = 500
-reps = 20
-
-id_list = list(
-  real = list(
-    adult = 1590,
-    video_transcoding = 44974,
-    sgemm_gpu = 44961,
-    physiochemical_protein = 44963,
-    diamonds = 44974
-  ),
-  simulated = list(
-    adult = 45689,
-    video_transcoding = 45696,
-    sgemm_gpu = 45695,
-    physiochemical_protein = 45694,
-    diamonds = 45692
-  )
+info = list(
+  list(name = "adult", id = 1590),
+  list(name = "video_transcoding", id = 44974),
+  list(name = "sgemm_gpu", id = 44961),
+  list(name = "physiochemical_protein", id = 44963),
+  list(name = "diamonds", id = 44974)
+  # list(name = "simul_adult", id = 45689),
+  # list(name = "simul_video_transcoding", id = 45696),
+  # list(name = "simul_sgemm_gpu", id = 45695),
+  # list(name = "simul_physiochemical_protein", id = 45694),
+  # list(name = "simul_diamonds", id = 45692)
 )
 
-tasks_list = imap(id_list, function(ids, type) {
-  imap(ids, function(id, nm) {
-    odata = odt(id, parquet = TRUE)
 
-    odata = odt(id, parquet = TRUE)
-    ids = if (odata$nrow == 5100000) {
-      1:100000
-    } else if (nm == "adult") {
-      (1:odata$nrow)[complete.cases(odata$data)]
-    } else {
-      1:odata$nrow
-    }
-    target = odata$target_names
-    backend = as_data_backend(odata)
+f = function(info, size, reps) {
+  nm = info$name
+  id = info$id
+  odata = odt(id, parquet = TRUE)
 
-    tmpdata = backend$data(ids, backend$colnames)
-    names(tmpdata)[names(tmpdata) == "mlr3_row_id"] = "..row_id"
-    backend = as_data_backend(tmpdata, primary_key = "..row_id")
-    task = if (is.factor(tmpdata[[target]])) {
-      as_task_classif(backend, target = target)
-    } else {
-      as_task_regr(backend, target = target)
-    }
-    task$id = paste0(type, "_", nm)
+  ids = if (odata$nrow == 5100000) {
+    1:100000
+  } else if (nm == "adult") {
+    (1:odata$nrow)[complete.cases(odata$data)]
+  } else {
+    1:odata$nrow
+  }
+  target = odata$target_names
+  backend = as_data_backend(odata)
 
-    task
-  })
-})
+  tmpdata = backend$data(ids, backend$colnames)
+  names(tmpdata)[names(tmpdata) == "mlr3_row_id"] = "..row_id"
+  backend = as_data_backend(tmpdata, primary_key = "..row_id")
+  task = if (is.factor(tmpdata[[target]])) {
+    as_task_classif(backend, target = target)
+  } else {
+    as_task_regr(backend, target = target)
+  }
+  task$id = nm
 
-ho_tasks_list = map(tasks_list, function(tasks) {
-  map(tasks, function(task) {
-    ho_task = task$clone(deep = TRUE)$filter(sample(task$row_ids, 10000L))
-    task$filter(setdiff(task$row_ids, ho_task$row_ids))
-    return(ho_task)
-  })
-})
+  ho_task = task$clone(deep = TRUE)$filter(sample(task$row_ids, 10000L))
+  task$filter(setdiff(task$row_ids, ho_task$row_ids))
 
-tbl = rbindlist(pmap(list(tasks = tasks_list, ho_tasks = ho_tasks_list), function(tasks, ho_tasks) {
-  tbl = rbindlist(pmap(list(task = tasks, ho_task = ho_tasks), function(task, ho_task) {
-    learner = switch(task$task_type,
-      regr = lrn("regr.rpart"),
-      classif = lrn("classif.rpart")
-    )
-    learner = as_learner(ppl("robustify", learner = learner, task = task) %>>% learner)
-    learner$id = "rpart"
-    rbindlist(map(seq_len(reps), function(repl) {
-      task = task$clone(deep = TRUE)
-      task$filter(sample(task$row_ids, size = 1000L))
-      rr = resample(task, learner, rsmp("holdout"))
-      ci = infer_holdout(rr)
-      learner$train(task)
-      risk = learner$predict(ho_task)$score()
-      cbind(ci, data.table(risk = risk, task = task$id, repl = repl, size = size))
-    }))
+  learner = switch(task$task_type,
+    regr = lrn("regr.lm"),
+    classif = lrn("classif.log_reg")
+  )
+  learner = as_learner(ppl("robustify") %>>% learner)
+  learner$id = "linear"
+
+  tbl = rbindlist(map(seq_len(reps), function(repl) {
+    task = task$clone(deep = TRUE)
+    task$filter(sample(task$row_ids, size = size))
+    rr = resample(task, learner, rsmp("cv"))
+    ci = infer_bayle(rr)
+    learner$train(task)
+    risk = learner$predict(ho_task)$score()
+    cbind(ci, data.table(risk = risk, task = task$id, repl = repl, size = size))
   }))
-}))
 
+  tbl[, let(
+    hit = lower <= risk & upper >= risk,
+    width = upper - lower
+  )]
 
-tbl[, let(
-  hit = lower <= risk & upper >= risk
-)]
+  tbl
+}
 
-tbl = tbl[, list(cov = mean(hit)), by = "task"]
+makeRegistry(
+  # "/gscratch/sfische6/benchmarks/ci_for_ge/real_vs_simul",
+  NA,
+  packages = c("mlr3", "mlr3learners", "mlr3pipelines", "mlr3db", "inferGE", "mlr3oml", "mlr3misc", "here", "duckdb", "DBI", "lgr", "data.table"),
+)
+
+batchMap(fun = f, info = info, more.args = list(reps = 100, size = 1000))
+
+testJob(1)
